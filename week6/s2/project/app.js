e7ffadad6fd687079700cd022ac9d10010d220e6
arsham <arshamgamer85@gmail.com>
2021-08-21 15:42:16 +0430

    init

---
+++ week6/s2/project/app.js
@@ -0,0 +1,16 @@
+var number1 = window.prompt("enter 1 number between 0 to 20 :")
+var number2 = window.prompt("enter 1 number between 0 to 20 :")
+var number3 = window.prompt("enter 1 number between 0 to 20 :")
+var plus = (number1 + number2 + number3)
+var division = (plus / 3)
+if (division >= 10) {
+    alert ("pass")
+}
+if (division <= 10) {
+    alert ("fail")
+}
+
+
+
+
+
