5b3ee2bd3df8f74735f3b431ba85dd5ec2239cb4
arsham <arshamgamer85@gmail.com>
2021-08-27 21:42:15 +0430

    init

---
+++ week 8/s1/class/app.js
@@ -0,0 +1,14 @@
+
+var num = prompt('type your number')
+var remider = num % 2
+if (remider == 0){
+    alert('zog')
+}
+else if (remider == 1){
+    alert('fard')
+}
+else{
+    alert('please enter a number')
+}
+
+
